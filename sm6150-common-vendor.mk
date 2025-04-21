7f3eb6be84c0d30a671e1d49aa86278560787b1b
romgharti <romgharti@gmail.com>
2025-04-21 20:42:03 +0545

    sm6150-common: Restore open source soundtrigger 2.3
    Signed-off-by: romgharti <romgharti@gmail.com>

--- sm6150-common-vendor.mk
+++ sm6150-common-vendor.mk
@@ -177,7 +177,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vpp@1.1 \
     vendor.qti.hardware.vpp@1.2 \
     vendor.qti.hardware.vpp@1.3 \
-    sound_trigger.primary.sm6150 \
     vendor.qti.hardware.bluetooth_audio@2.0-impl \
     libadpcmdec \
     libcppf \
